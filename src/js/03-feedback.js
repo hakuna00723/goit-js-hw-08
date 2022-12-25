caf64225e9a1ba19fc4a4b18dc77f82f8f7b89f8
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-25 21:51:03 +0100

    03feedback-done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import '../css/common.css';
+import '../css/03-feedback.css';
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.input');
+const storage = 'feedback-form-state';
+const formEntries = {};
+
+accumulateTextarea();
+
+form.addEventListener(
+  'input',
+  throttle(event => {
+    formEntries[event.target.name] = event.target.value;
+    const stringifiedData = JSON.stringify(formEntries);
+    localStorage.setItem(storage, stringifiedData);
+  }, 500)
+);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  event.currentTarget.reset();
+  const object = JSON.parse(localStorage.getItem(storage));
+  localStorage.removeItem(storage);
+});
+
+function accumulateTextarea() {
+  const savedMessage = JSON.parse(localStorage.getItem(storage));
+  if (savedMessage === null) {
+    return;
+  }
+  textarea.value = savedMessage['message'] || '';
+  input.value = savedMessage['email'] || '';
+}
