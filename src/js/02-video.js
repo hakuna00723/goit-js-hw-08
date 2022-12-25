8f278caef1e7cb4ca887be951c2faa6e4edc337b
Serhii <110534352+hakuna00723@users.noreply.github.com>
2022-12-25 06:34:51 +0100

    02video-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+console.log(Player);
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(currentTime, 1000));
+
+function currentTime(time) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(time.seconds)
+  );
+}
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('Wrong time!');
+        break;
+      default:
+        console.log('Error!');
+        break;
+    }
+  });
